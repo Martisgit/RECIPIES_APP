00d952773ebea13bfaaca51313cbda3073eed80d
martisgit <martisgit@github.com>
2024-11-13 10:04:01 +0200

    changes

--- src/model/recipes.js
+++ src/model/recipes.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const recipeSchema = mongoose.Schema({
   id: { type: String, required: true },
   name: { type: String, required: true },
-  ingredients: { type: [String], required: true },
+  ingredients: { type: String, required: true },
   description: { type: String, required: true },
   rating: { type: Number, required: true },
 });
